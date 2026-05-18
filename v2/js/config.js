34eceaa9646f3e9ff55fe36f39763a68985488d9
weberpbj <weberp.bj@gmail.com>
2026-05-18 22:55:12 +0200

    Update config.js

--- v2/js/config.js
+++ v2/js/config.js
@@ -5,7 +5,7 @@ var WEBERP_CONFIG = {
   API_URL: 'https://script.google.com/macros/s/AKfycbx_N97C9AKyBQzn3v3f8OAHBbYayBnEbYyzuH0aOIQ6TSUAP0oUrv2Z3IhhondBmrQ5Ww/exec',
 
   // ── v2.0 TEST (novi Apps Script deployment URL — upiši kada deployas) ──
-  API_URL_V2: 'https://script.google.com/macros/s/AKfycbxBBqrFj_rmzrhvQ31iIf8BzRTLBFaMNQVwJe8cbEidQDkRrHKXTwKXhPMtgF-2VaTbew/exec',
+  API_URL_V2: 'https://script.google.com/macros/s/AKfycbyGc7wB8zybRr2ncWXe91w5i6hhvlSdzcbNu0HGcheYetYn5wfCXshjdB0JsOLPn-16Xg/exec',
 
   APP_NAME:  'weberp-bj',
   APP_VER:   'v2.0',
