e5e84757295b97be25f8b660ea981942249fb6a6
weberpbj <weberp.bj@gmail.com>
2026-05-20 22:39:41 +0200

    Add files via upload

---
+++ v3/js/config.js
@@ -0,0 +1,8 @@
+// weberp-bj v3.0 — config.js
+var WEBERP_CONFIG = {
+  API_URL_V2: 'https://script.google.com/macros/s/AKfycbxBBqrFj_rmzrhvQ31iIf8BzRTLBFaMNQVwJe8cbEidQDkRrHKXTwKXhPMtgF-2VaTbew/exec',
+  APP_NAME:  'weberp-bj',
+  APP_VER:   'v3.0',
+  TVRTKA:    'weberp-bj',
+  get ACTIVE_API_URL() { return this.API_URL_V2; }
+};
